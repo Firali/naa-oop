216fa12d70f38a878cbdc8a7bb5095a03e9d8afd
Student <student@naa.local>
2022-03-01 10:31:24 +0400

    continue

--- lab/01/calculator.py
+++ lab/01/calculator.py
@@ -1,1 +1,21 @@
 print('Salam')
+ def add(num1 , num2):
+     return num1 + num2
+def sub(num1 ,num2):
+     return num1*num2:
+def multiply(num1 ,num2):
+     return num1 - num2
+def divide(num1 ,num2):
+    return num1/num2
+
+     print(Select your operation :
+     ' 1. add '
+     ' 2. sub '
+     ' 3. multiply '
+     ' 4. divide  ' ) 
+
+     
+
+
+
+            
